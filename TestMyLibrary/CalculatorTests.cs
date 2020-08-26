b99b486eccf4e28637e80882c45238c1d36c0f2a
shuheydev <shuhey.dev@gmail.com>
2020-08-26 16:21:36 +0900

    Modified test case's signature

--- TestMyLibrary/CalculatorTests.cs
+++ TestMyLibrary/CalculatorTests.cs
@@ -11,10 +11,9 @@ namespace TestMyLibrary.Tests
         [InlineData(4, 3, 7)]
         [InlineData(21, 5.25, 26.25)]
         [InlineData(double.MaxValue, 5, double.MaxValue)]//edge case
-        public void Add_SimpleValuesShouldCalculate(double x, double y, double result)
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
         {
             //Arrange
-            double expected = result;
 
             //Act
             double actual = Calculator.Add(x, y);
